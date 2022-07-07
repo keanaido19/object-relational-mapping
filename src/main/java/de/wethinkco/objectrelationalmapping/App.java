3e8fe51fe00a7c6807fb5c7baa9a4959d0cafa22
keanaido021@ <keanaido021@student.wethinkcode.co.za>
2022-07-07 18:53:05 +0200

    initial commit

---
+++ src/main/java/de/wethinkco/objectrelationalmapping/App.java
@@ -0,0 +1,13 @@
+package de.wethinkco.objectrelationalmapping;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
